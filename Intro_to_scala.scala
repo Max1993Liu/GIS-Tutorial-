1d4fee884932a99648285768d44c70e53324c45b
Max Liu <zl2480@columbia.edu>
2017-02-20 13:16:59 -0500

    update

--- Intro_to_scala.scala
+++ Intro_to_scala.scala
@@ -499,3 +499,64 @@ echo("A", "B", "C")
 val arr = Array("A", "B", "C")
 echo(arr) //get error
 echo(arr: _*) //expand the arguments by : _*
+
+
+--- High order functions -----------------------------------------
+--- Functions that take function as input ------------------------
+object FileMatch {
+  private def filesHere = (new java.io.File(".")).listFiles
+
+  def filesEnding(query: String) = {
+    for (file <- filesHere; if file.getName.endsWith(query))
+      yield file
+  }
+
+  def filesContaining(query: String) = {
+    for (file <- filesHere; if file.getName.contains(query))
+      yield file
+  }
+  
+  def filesRegex(query: String) = {
+    for (file <- filesHere; if file.getName.matches(query))
+      yield file
+  }
+
+}
+
+
+//instead of passing in three different functions for searching
+//you can pass in a function value instead
+def filesMatching(query: String, matcher: (String, String) => Boolean) = {
+  for (file <- filesHere, if matcher(file.getName, query))
+    yield file
+}
+
+def filesEnding(query: String) = {
+  filesMatching(query, _.endsWith(_))
+  //This is exactly the same as
+  filesMatching((query, (fileName: String, query: String) => fileName.endsWith(query)))
+}
+
+def filesContaining(query: String) = {
+  filesMatching(query, _.contains(_))
+}
+
+def filesRegex(query: String) = {
+  filesMatching(query, _.matches(_))
+}
+
+//This can be further simplified
+object FileMatcher {
+  private def fileHere = (new java.io.File(".")).listFiles
+  
+  private def filesMatching(matcher: String => Boolean) = {
+    for (file <- fileHere; if matcher(file.getName))
+      yield file
+  }
+  
+  def filesEnding(query: String) = {
+    filesMatching(_.endsWith(query))
+  }
+}
+
+--- Currying -----------------------------------------------
